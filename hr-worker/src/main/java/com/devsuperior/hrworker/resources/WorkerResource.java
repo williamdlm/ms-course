2f4e804fefa08adb4e81842861befe50adf7e31e
William David de Lima Mota <williammota.david@gmail.com>
2023-04-17 05:31:23 -0300

    Hystrix fault tolerance

--- hr-worker/src/main/java/com/devsuperior/hrworker/resources/WorkerResource.java
+++ hr-worker/src/main/java/com/devsuperior/hrworker/resources/WorkerResource.java
@@ -18,27 +18,31 @@ import com.devsuperior.hrworker.repositories.WorkerRepository;
 @RestController
 @RequestMapping(value = "/workers")
 public class WorkerResource {
-	
+
 	private static Logger logger = LoggerFactory.getLogger(WorkerResource.class);
-	
-	
+
 	@Autowired
 	private Environment env;
-	
+
 	@Autowired
 	private WorkerRepository repository;
-	
+
 	@GetMapping
-	public ResponseEntity<List<Worker>> findAll(){
+	public ResponseEntity<List<Worker>> findAll() {
 		List<Worker> list = repository.findAll();
 		return ResponseEntity.ok(list);
 	}
-	
+
 	@GetMapping(value = "/{id}")
-	public ResponseEntity<Worker> findById(@PathVariable Long id){
-		
+	public ResponseEntity<Worker> findById(@PathVariable Long id) {
+
+		/*
+		 * try { Thread.sleep(3000L); } catch (InterruptedException e) { // TODO
+		 * Auto-generated catch block e.printStackTrace(); }
+		 */
+
 		logger.info("PORT = " + env.getProperty("local.server.port"));
-		
+
 		Worker obj = repository.findById(id).get();
 		return ResponseEntity.ok(obj);
 	}
