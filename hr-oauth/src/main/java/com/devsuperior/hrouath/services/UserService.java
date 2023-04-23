5e056bea983655a1ef12191e480d8ef411df4f2b
William David de Lima Mota <williammota.david@gmail.com>
2023-04-22 23:08:46 -0300

    UserFeignClient

---
+++ hr-oauth/src/main/java/com/devsuperior/hrouath/services/UserService.java
@@ -0,0 +1,29 @@
+package com.devsuperior.hrouath.services;
+
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+
+import com.devsuperior.hrouath.entities.User;
+import com.devsuperior.hrouath.feignclients.UserFeignClient;
+
+@Service
+public class UserService {
+
+	private static Logger logger = LoggerFactory.getLogger(UserService.class);
+
+	@Autowired
+	private UserFeignClient userFeignClient;
+
+	public User findByEmail(String email) {
+		User user = userFeignClient.findByEmail(email).getBody();
+		if (user == null) {
+			logger.error("Email not found: " + email);
+			throw new IllegalArgumentException("Email not found");
+		}
+		logger.info("Email found: " + email);
+		return user;
+	}
+
+}
